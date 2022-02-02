8784d97f1e86c6ee94e2557d3ae2477780f01e73
Claudio Reis <58343852+claudioreisjr@users.noreply.github.com>
2022-02-02 00:55:57 -0300

    Add classes via upload

---
+++ classeB.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+//trabalho git engenharia de software
+//Claudio M dos Reis Junior
+//Classe B
+
+class B{
+	private:
+	int x;
+	float y;
+	
+	public:
+	int B1;
+	float B2;
+	//int A1;
+	//float A2;
+	
+	
+    void setB1(int B1){
+      x = B1;
+    }
+    void setB2(float B2){
+	  y = B2;
+	}      
+    int getB1(){
+      return B1;
+   }
+   float getB2(){
+	  return B2;
+   }
+};
+
+	
+//metodos classe A
+void MB1(){
+	cout << "Metodo MB1" << endl;
+}
+
+void MB2(){
+	cout << "Metodo MB2" << endl;
+}
+
+int main(){
+	B eng;
+	cin >> eng.B1 >> eng.B2;//leitura entradas class
+	
+	//eng.setA1(12);
+	//eng.setA2(1.2);
+	
+      
+    cout << eng.getB1() << endl;
+    cout << eng.getB2() << endl;
+	
+	MB1();
+	MB2();
+
+	return 0;
+}
